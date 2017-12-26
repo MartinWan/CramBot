f91c961cd0e280b51c393904afee12a551d3ff43
Martin Wan <mlwan@uvic.ca>
2017-12-26 14:03:16 -0800

    Remove unused SERVER_URL environment variable

--- app.js
+++ app.js
@@ -26,9 +26,8 @@ app.use(express.static('public'))
 const APP_SECRET = process.env.MESSENGER_APP_SECRET
 const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN
 const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN
-const SERVER_URL = process.env.SERVER_URL
 
-if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
   console.error("Missing config values")
   process.exit(1)
 }
